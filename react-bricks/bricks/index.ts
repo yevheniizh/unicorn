2c322e7077c5a1dcc16f7edf6c9cdc27e652fa6e
yevhenii-zhyrov <yevhenii.zhyrov@authenticpartners.live>
2023-05-14 10:54:30 +0300

    first commit

---
+++ react-bricks/bricks/index.ts
@@ -0,0 +1,19 @@
+import { types } from 'react-bricks/frontend'
+
+import HeroUnit from './custom/MyHeroUnit'
+import reactBricksUITheme from './react-bricks-ui'
+
+const bricks: types.Theme[] = [
+  reactBricksUITheme, // React Bricks UI
+  {
+    themeName: 'Custom Theme 1',
+    categories: [
+      {
+        categoryName: 'Hero sections',
+        bricks: [HeroUnit], // Custom Bricks
+      },
+    ],
+  },
+]
+
+export default bricks
---
+++ react-bricks/pageTypes.ts
@@ -0,0 +1,47 @@
+import { types } from 'react-bricks/frontend'
+
+const pageTypes: types.IPageType[] = [
+  {
+    name: 'page',
+    pluralName: 'pages',
+    defaultLocked: false,
+    defaultStatus: types.PageStatus.Published,
+    getDefaultContent: () => [],
+  },
+  {
+    name: 'blog',
+    pluralName: 'Blog',
+    defaultLocked: false,
+    defaultStatus: types.PageStatus.Published,
+    getDefaultContent: () => [],
+    allowedBlockTypes: [
+      'title',
+      'paragraph',
+      'big-image',
+      'video',
+      'code',
+      'tweet',
+      'tweet-light',
+      'blog-title',
+      'newsletter-subscribe',
+      'external-data-example',
+    ],
+    getExternalData: () =>
+      fetch('https://catfact.ninja/fact')
+        .then((response) => response.json())
+        .then((data) => ({
+          catFact: data.fact,
+        })),
+  },
+
+  {
+    name: 'layout',
+    pluralName: 'layout',
+    defaultLocked: false,
+    defaultStatus: types.PageStatus.Published,
+    getDefaultContent: () => [],
+    isEntity: true,
+  },
+]
+
+export default pageTypes
